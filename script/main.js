85be7b790f6e87276df84bc646c975e27c618fdf
Nataliya Satsyuk <natalia.satsyuk@gmail.com>
2016-08-17 11:28:36 +0300

    .

--- script/main.js
+++ script/main.js
@@ -8,30 +8,10 @@ $('.slider-wrap').slick({
   autoplay: true,
   autoplaySpeed: 2000,
   arrows: false
-  // pauseOnFocus: false
-  // pauseOnHover: false
-  // swipe: false
-  // swipeToSlide:true
-  // waitForAnimate:false
-  
+   
 });
 
 
-
-// $(".menu-small-icon").click(function(){
-// 	$(".main-list").slideToggle();
-// });
-
-// $(window).resize(function(){
-// 	var winwidth = $(window).innerWidth();
-// 	if(winwidth < 992){
-// 		$(".main-list").css("display", "none")
-// 	}
-// 	if(winwidth > 992){
-// 		$(".main-list").css("display", "inline-block"),
-// 		$(".main-list").css("text-aline", "right")
-// 	}
-// });
 
 $(".menu-small-icon").click(function(){
 	$(".main-list").slideToggle();
@@ -44,7 +24,5 @@ $(window).resize(function(){
 	}
 	if(winwidth > 992){
 		$(".main-list").css("display", "inline-block")
-		// $(".main-list").addClass("text-right")
-	
 	}
 });
